9751c30a84ae65555848c1058195f82195a6e456
Daryna Rudenko <daryna.rudenko@gmail.com>
2025-12-23 22:02:26 +0300

    Повернула загублені компоненти header та footer, почала робити лаб-2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'front_lab_1';
+  title = 'Toy Store';
 }
---
+++ src/shared/models/item_model.ts
@@ -0,0 +1,10 @@
+export interface ItemModel {
+    id: string;
+    name: string;
+    description: string;
+    price: number;
+    color: string;
+    material: string;
+    inStock: boolean;
+    imageUrl: string;
+}
